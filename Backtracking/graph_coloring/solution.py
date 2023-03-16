4c3fb9eaab6fee96d7f5c84df4e2cdb190dc40a3
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2023-03-16 15:24:04 +0100

    second commit

--- Backtracking/graph_coloring/solution.py
+++ Backtracking/graph_coloring/solution.py
@@ -1,23 +1,69 @@
+import random
 class GraphColoring_backtracking:
-    def __init__(self, colors,n):
-        self.n = n
-        self.MA = []
-        self.colors = colors
-        self.result = []
-    #self.MA.append(list(map(int, input().split())))
+    def __init__(self,n,m):
+        self.n=n #number of vertices
+        self.m=m #number of colors
+        self.graph=[] #adjacent matrcice
+        self.result=[] # list of list of all faisable solution
+    #get the graph from the keyboard
+    #choice =0 =>non directed graph
+    def get_adjacent_matrix(self, choice=0):
+        if choice == 0:
+            for i in range(self.n):
+                self.graph[i][i]=0
+                for j in range(i):
+                    self.graph[i][j]=input('Enter the weight between node %s and node%s '%(i+1,j+1))
+                    self.graph[j][i]=self.graph[i][j]
+        else:
+            for i in range(self.n):
+                for j in range(self.n):
+                    self.graph[i][j]=input('Enter the weight between node %s and node%s '%(i+1,j+1))
+                    
+    #generate the graph randomly
+    def generate_adjacent_matrix(self, choice=0):
+        if choice == 0:
+            for i in range(self.n):
+                self.graph[i][i]=0
+                for j in range(i):
+                    self.graph[i][j]=random.randint(0,100)
+                    self.graph[j][i]=self.graph[i][j]
+        else:
+            for i in range(self.n):
+                for j in range(self.n):
+                    self.graph[i][j]=random.randint(0,100)
 
-    def get_adjancent_matrix(self,graph_type=0):
-        #the graph is non directed
-        if graph_type == 0:
-            born_sup=i
-        else: #the graph is directed
-            born_sup=self.n
+    def generate_random_adjacent_matrix_numpy(self):
+        import numpy as np
+        self.graph=np.random.randint(0,100,(self.n,self.n))
         for i in range(self.n):
-                self.MA[i][i]=0
-                for j in range(i+1,born_sup):
-                    self.MA[i][j]=input(f'Enter the value of the edge between {i+1} and {j+1} : ')
-    def generate_adjancent_matrix(self):
+            self.graph[i][i]=0
+
+    def is_acceptable_color_v1(self,node_index,color,possible_solution ):
+        for j in range(self.n):
+            if j!=node_index and self.graph[node_index][j]!=0 and possible_solution[node_index][j]==color:
+                return False
+        return True
+
+    def is_acceptable_color(self,possible_solution ):
+        index=len(possible_solution)-1
+        for i in range(len(possible_solution)-1):
+            if possible_solution[i]==possible_solution[-1] and self.graph[index][i]!=0:
+                return False
+        return True
+    def solve(self,possible_solution):
+        if len(possible_solution)==self.n:
+            pass
+        else:
+            pass
+    def print_solutions(self,):
         pass
-    def is_acceptable_color
-    def solve(self):
-        pass
+
+#testing
+n=int(input('Enter the number of vertices'))
+m=int(input('Enter the number of colors'))
+gc=GraphColoring_backtracking(n,m)
+gc.generate_adjacent_matrix() # non directed graph
+gc.solve()
+print('There are %s possible solutions'%len(gc.result))
+gc.print_solutions()
+    
